a97d315b6535e67b93aa299819369cc3927592cc
Jose A. Devia <totes111167@gmail.com>
2020-08-27 20:03:04 -0500

    New updates for Heroku deployment

--- message_board_project/settings.py
+++ message_board_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '1^y)p)8cwy-7n7ym)#^qm%cms$%af51eyifb3nl+9-esfhdxl_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
